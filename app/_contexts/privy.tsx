335c77033937570e745af963166afe9a15bc3bff
CDCODE <CDCODE02@gmail.com>
2025-02-01 22:29:18 +0100

    cdcode image updates

--- app/_contexts/privy.tsx
+++ app/_contexts/privy.tsx
@@ -1,41 +1,41 @@
 "use client";
 
-import {PrivyProvider as PrivyProviderBase} from '@privy-io/react-auth';
-import {toSolanaWalletConnectors} from '@privy-io/react-auth/solana';
+import { PrivyProvider as PrivyProviderBase } from "@privy-io/react-auth";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const solanaConnectors = toSolanaWalletConnectors({
-    shouldAutoConnect: false,
+  shouldAutoConnect: false,
 });
 
 export const PrivyProvider: React.FC<Props> = ({ children }) => {
-    return (
-        <PrivyProviderBase
-            appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-            config={{
-                appearance: {
-                    theme: 'dark',
-                    accentColor: '#d19900',
-                    logo: 'https://www.askthehive.ai/logo-dark.png',
-                    walletChainType: 'solana-only',
-                },
-                externalWallets: {
-                    solana: {
-                        connectors: solanaConnectors
-                    }
-                },
-                solanaClusters: [
-                    {
-                        name: 'mainnet-beta',
-                        rpcUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL!,
-                    }
-                ]
-            }}
-        >
-            {children}
-        </PrivyProviderBase>
-    )
-}
+  return (
+    <PrivyProviderBase
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      config={{
+        appearance: {
+          theme: "dark",
+          accentColor: "#2FFF2B",
+          logo: "https://i.ibb.co/whCBtsGt/favcom.png",
+          walletChainType: "solana-only",
+        },
+        externalWallets: {
+          solana: {
+            connectors: solanaConnectors,
+          },
+        },
+        solanaClusters: [
+          {
+            name: "mainnet-beta",
+            rpcUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL!,
+          },
+        ],
+      }}
+    >
+      {children}
+    </PrivyProviderBase>
+  );
+};
