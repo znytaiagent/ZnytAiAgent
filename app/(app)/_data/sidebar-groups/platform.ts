2421fcb6f14d8408b3a2d9fd708c9702f3f0c9a8
CDCODE <CDCODE02@gmail.com>
2025-02-14 15:10:04 +0100

    cdcode link update fix

--- app/(app)/_data/sidebar-groups/platform.ts
+++ app/(app)/_data/sidebar-groups/platform.ts
@@ -1,30 +1,24 @@
 import { SidebarGroup } from "../../_types/sidebar";
 
 export const platformGroup: SidebarGroup = {
-    label: 'Platform',
-    items: [
-        {
-            icon: 'MessageSquare',
-            label: 'Chat',
-            href: '/chat'
-        },
-        {
-            icon: 'Book',
-            label: 'Docs',
-            href: 'https://docs.askthehive.ai',
-            external: true
-        },
-        {
-            icon: 'Twitter',
-            label: 'Follow Us',
-            href: 'https://x.com/askthehive_ai',
-            external: true
-        },
-        {
-            icon: 'Github',
-            label: 'Source Code',
-            href: 'https://github.com/jasonhedman/the-hive',
-            external: true
-        }
-    ]
-}
+  label: "Platform",
+  items: [
+    {
+      icon: "MessageSquare",
+      label: "Chat",
+      href: "/chat",
+    },
+    {
+      icon: "Twitter",
+      label: "Follow Us",
+      href: "https://x.com/ZnytAiAgent",
+      external: true,
+    },
+    {
+      icon: "Github",
+      label: "Source Code",
+      href: "https://github.com/znytaiagent",
+      external: true,
+    },
+  ],
+};
